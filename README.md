bb373702a5983468eadbd2e25982c880ec9b78fe
Mathijs Lemmers <mathijs750@gmail.com>
2024-10-15 21:12:37 +0200

    Add working game of life from section 4

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# webgpu
+# webGPU
+
+We are following along with
+https://codelabs.developers.google.com/your-first-webgpu-app
+
+## Development
+
+```bash
+deno install
+deno task dev
+```
--- main.js
+++ main.js
@@ -1,24 +1,126 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+// Setup plumbing
+const canvas = document.querySelector("canvas");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+if (!navigator.gpu) {
+  throw new Error("WebGPU not supported on this browser.");
+}
 
-setupCounter(document.querySelector('#counter'))
+const adapter = await navigator.gpu.requestAdapter();
+if (!adapter) {
+  throw new Error("No appropriate GPUAdapter found.");
+}
+
+const device = await adapter.requestDevice();
+
+const context = canvas.getContext("webgpu");
+const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
+context.configure({
+  device: device,
+  format: canvasFormat,
+});
+
+// WebGPU stuff
+
+const GRID_SIZE = 4;
+
+// Create a uniform buffer that describes the grid.
+const uniformArray = new Float32Array([GRID_SIZE, GRID_SIZE]);
+const uniformBuffer = device.createBuffer({
+  label: "Grid Uniforms",
+  size: uniformArray.byteLength,
+  usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+});
+device.queue.writeBuffer(uniformBuffer, 0, uniformArray);
+
+const vertices = new Float32Array([
+  -0.8,
+  -0.8,
+  0.8,
+  -0.8,
+  0.8,
+  0.8,
+
+  -0.8,
+  -0.8,
+  0.8,
+  0.8,
+  -0.8,
+  0.8,
+]);
+
+const vertexBuffer = device.createBuffer({
+  label: "Cell vertices",
+  size: vertices.byteLength,
+  usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+});
+
+device.queue.writeBuffer(vertexBuffer, /*bufferOffset=*/ 0, vertices);
+
+const vertexBufferLayout = {
+  arrayStride: 8,
+  attributes: [
+    {
+      format: "float32x2",
+      offset: 0,
+      shaderLocation: 0, // Position, see vertex shader
+    },
+  ],
+};
+
+const cellShaderModule = device.createShaderModule({
+  label: "Cell shader",
+  code: /* wgsl */ `
+  @vertex
+  fn vertexMain(@location(0) pos: vec2f) ->
+    @builtin(position) vec4f {
+    return vec4f(pos, 0, 1);
+  }
+  
+
+    @fragment
+    fn fragmentMain() -> @location(0) vec4f {
+        return vec4f(1, 0.8, 0.3, 1); // (Red, Green, Blue, Alpha)
+    }
+  `,
+});
+
+const cellPipeline = device.createRenderPipeline({
+  label: "Cell pipeline",
+  layout: "auto",
+  vertex: {
+    module: cellShaderModule,
+    entryPoint: "vertexMain",
+    buffers: [vertexBufferLayout],
+  },
+  fragment: {
+    module: cellShaderModule,
+    entryPoint: "fragmentMain",
+    targets: [
+      {
+        format: canvasFormat,
+      },
+    ],
+  },
+});
+
+const encoder = device.createCommandEncoder();
+
+const pass = encoder.beginRenderPass({
+  colorAttachments: [
+    {
+      view: context.getCurrentTexture().createView(),
+      loadOp: "clear",
+      clearValue: { r: 0, g: 0.14, b: 0.12, a: 1 },
+      storeOp: "store",
+    },
+  ],
+});
+
+pass.setPipeline(cellPipeline);
+pass.setVertexBuffer(0, vertexBuffer);
+pass.draw(vertices.length / 2); // 6 vertices
+
+pass.end();
+// Finish the command buffer and immediately submit it.
+device.queue.submit([encoder.finish()]);
