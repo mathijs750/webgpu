7cc89866d362b7821a7e1b32146298c444b92db4
Mathijs Lemmers <mathijs750@gmail.com>
2024-10-19 21:34:14 +0200

    Add actual game of life

--- main.js
+++ main.js
@@ -168,10 +168,29 @@ const simulationShaderModule = device.createShaderModule({
 
     @compute @workgroup_size(${WORKGROUP_SIZE}, ${WORKGROUP_SIZE}) 
     fn computeMain(@builtin(global_invocation_id) cell: vec3u) {
-      if (cellStateIn[cellIndex(cell.xy)] == 1) {
-        cellStateOut[cellIndex(cell.xy)] = 0;
-      } else {
-        cellStateOut[cellIndex(cell.xy)] = 1;
+      let activeNeighbors = cellActive(cell.x+1, cell.y+1) +
+                            cellActive(cell.x+1, cell.y) +
+                            cellActive(cell.x+1, cell.y-1) +
+                            cellActive(cell.x, cell.y-1) +
+                            cellActive(cell.x-1, cell.y-1) +
+                            cellActive(cell.x-1, cell.y) +
+                            cellActive(cell.x-1, cell.y+1) +
+                            cellActive(cell.x, cell.y+1);
+
+
+      let i = cellIndex(cell.xy);
+
+      // Conway's game of life rules:
+      switch activeNeighbors {
+        case 2: { // Active cells with 2 neighbors stay active.
+          cellStateOut[i] = cellStateIn[i];
+        }
+        case 3: { // Cells with 3 neighbors become or stay active.
+          cellStateOut[i] = 1;
+        }
+        default: { // Cells with < 2 or > 3 neighbors become inactive.
+          cellStateOut[i] = 0;
+        }
       }
     }
   `,
