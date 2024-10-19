04cb7accd77ee1c6a28a183eb22d2fabbf2d1f82
Mathijs Lemmers <mathijs750@gmail.com>
2024-10-19 21:24:37 +0200

    Add compute shader and refactor bindings

--- main.js
+++ main.js
@@ -26,6 +26,7 @@ context.configure({
 
 // WebGPU stuff
 const GRID_SIZE = 32;
+const WORKGROUP_SIZE = 8;
 const UPDATE_INTERVAL = 200; // Update every 200ms (5 times/sec)
 let step = 0;
 
@@ -63,6 +64,30 @@ const vertexBufferLayout = {
     },
   ],
 };
+
+// Create the bind group layout and pipeline layout.
+const bindGroupLayout = device.createBindGroupLayout({
+  label: "Cell Bind Group Layout",
+  entries: [{
+    binding: 0,
+    // Add GPUShaderStage.FRAGMENT here if you are using the `grid` uniform in the fragment shader.
+    visibility: GPUShaderStage.VERTEX | GPUShaderStage.COMPUTE,
+    buffer: {}, // Grid uniform buffer
+  }, {
+    binding: 1,
+    visibility: GPUShaderStage.VERTEX | GPUShaderStage.COMPUTE,
+    buffer: { type: "read-only-storage" }, // Cell state input buffer
+  }, {
+    binding: 2,
+    visibility: GPUShaderStage.COMPUTE,
+    buffer: { type: "storage" }, // Cell state output buffer
+  }],
+});
+
+const pipelineLayout = device.createPipelineLayout({
+  label: "Cell Pipeline Layout",
+  bindGroupLayouts: [bindGroupLayout],
+});
 
 const cellShaderModule = device.createShaderModule({
   label: "Cell shader",
@@ -105,7 +130,7 @@ const cellShaderModule = device.createShaderModule({
 
 const cellPipeline = device.createRenderPipeline({
   label: "Cell pipeline",
-  layout: "auto",
+  layout: pipelineLayout,
   vertex: {
     module: cellShaderModule,
     entryPoint: "vertexMain",
@@ -119,6 +144,40 @@ const cellPipeline = device.createRenderPipeline({
         format: canvasFormat,
       },
     ],
+  },
+});
+
+// Create the compute shader that will process the simulation.
+const simulationShaderModule = device.createShaderModule({
+  label: "Game of Life simulation shader",
+  code: /* wgsl */ `
+    @group(0) @binding(0) var<uniform> grid: vec2f;
+
+    @group(0) @binding(1) var<storage> cellStateIn: array<u32>;
+    @group(0) @binding(2) var<storage, read_write> cellStateOut: array<u32>;
+
+    fn cellIndex(cell: vec2u) -> u32 {
+      return cell.y * u32(grid.x) + cell.x;
+    }
+
+    @compute @workgroup_size(${WORKGROUP_SIZE}, ${WORKGROUP_SIZE}) 
+    fn computeMain(@builtin(global_invocation_id) cell: vec3u) {
+      if (cellStateIn[cellIndex(cell.xy)] == 1) {
+        cellStateOut[cellIndex(cell.xy)] = 0;
+      } else {
+        cellStateOut[cellIndex(cell.xy)] = 1;
+      }
+    }
+  `,
+});
+
+// Create a compute pipeline that updates the game state.
+const simulationPipeline = device.createComputePipeline({
+  label: "Simulation pipeline",
+  layout: pipelineLayout,
+  compute: {
+    module: simulationShaderModule,
+    entryPoint: "computeMain",
   },
 });
 
@@ -164,32 +223,48 @@ device.queue.writeBuffer(cellStateStorage[1], 0, cellStateArray);
 const bindGroups = [
   device.createBindGroup({
     label: "Cell renderer bind group A",
-    layout: cellPipeline.getBindGroupLayout(0),
+    layout: bindGroupLayout, // Updated Line
     entries: [{
       binding: 0,
       resource: { buffer: uniformBuffer },
     }, {
       binding: 1,
       resource: { buffer: cellStateStorage[0] },
+    }, {
+      binding: 2,
+      resource: { buffer: cellStateStorage[1] },
     }],
   }),
   device.createBindGroup({
     label: "Cell renderer bind group B",
-    layout: cellPipeline.getBindGroupLayout(0),
+    layout: bindGroupLayout, // Updated Line
     entries: [{
       binding: 0,
       resource: { buffer: uniformBuffer },
     }, {
       binding: 1,
       resource: { buffer: cellStateStorage[1] },
+    }, {
+      binding: 2,
+      resource: { buffer: cellStateStorage[0] },
     }],
   }),
 ];
 
 function updateGrid() {
-  step++;
-
   const encoder = device.createCommandEncoder();
+
+  const computePass = encoder.beginComputePass();
+
+  computePass.setPipeline(simulationPipeline);
+  computePass.setBindGroup(0, bindGroups[step % 2]);
+
+  const workgroupCount = Math.ceil(GRID_SIZE / WORKGROUP_SIZE);
+  computePass.dispatchWorkgroups(workgroupCount, workgroupCount);
+
+  computePass.end();
+
+  step++;
 
   const pass = encoder.beginRenderPass({
     colorAttachments: [
