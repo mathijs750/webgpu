75acd4c930d30e0b96b2e802929d6dd446829319
Mathijs Lemmers <mathijs750@gmail.com>
2024-10-19 12:38:09 +0200

    add grid cell colors

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ context.configure({
 });
 
 // WebGPU stuff
-const GRID_SIZE = 4;
+const GRID_SIZE = 32;
 
 const vertices = new Float32Array([
   -0.8,
@@ -65,24 +65,36 @@ const vertexBufferLayout = {
 const cellShaderModule = device.createShaderModule({
   label: "Cell shader",
   code: /* wgsl */ `
+    struct VertexInput {
+      @location(0) pos: vec2f,
+      @builtin(instance_index) instance: u32,
+    };
+
+    struct VertexOutput {
+      @builtin(position) pos: vec4f,
+      @location(0) cell: vec2f,
+    };
+
     @group(0) @binding(0) var<uniform> grid: vec2f;
 
     @vertex
-    fn vertexMain(@location(0) pos: vec2f,
-                  @builtin(instance_index) instance: u32) ->
-      @builtin(position) vec4f {
+    fn vertexMain(input: VertexInput) -> VertexOutput {
 
-      let i = f32(instance); // Save the instance_index as a float
+      let i = f32(input.instance); // Save the instance_index as a float
       let cell = vec2f(i % grid.x, floor(i / grid.x));
       let cellOffset = cell / grid * 2;
-      let gridPos = (pos + 1) / grid - 1 + cellOffset;
+      let gridPos = (input.pos + 1) / grid - 1 + cellOffset;
 
-      return vec4f(gridPos, 0, 1);
+      var output: VertexOutput;
+      output.pos = vec4f(gridPos, 0, 1);
+      output.cell = cell;
+      return output;
     }
 
     @fragment
-    fn fragmentMain() -> @location(0) vec4f {
-        return vec4f(1, 0.8, 0.3, 1); // (Red, Green, Blue, Alpha)
+    fn fragmentMain(input: VertexOutput) -> @location(0) vec4f {
+        // return vec4f(1, 0.8, 0.3, 1); // (Red, Green, Blue, Alpha)
+        return vec4f(input.cell / grid, 0, 1); // (Red, Green, Blue, Alpha)
     }
   `,
 });
